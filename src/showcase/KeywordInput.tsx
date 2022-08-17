91d164f60704a97ba2f0ad347a95d6b84f99d68c
Heesoo <cleanior@gmail.com>
2022-08-18 01:13:45 +0900

    Updates

--- src/showcase/KeywordInput.tsx
+++
@@ -1,7 +0,0 @@
-import { ForwardedRef, forwardRef } from "react";
-
-const KeywordInput = forwardRef((_, ref: ForwardedRef<HTMLInputElement>) => {
-    return <input type="search" required placeholder="Write to search ..." ref={ref} />;
-});
-
-export default KeywordInput;
